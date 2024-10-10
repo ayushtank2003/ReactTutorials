1004b797631a99a9fcd04b1a03cb768457a44986
ayushtank2003 <ayushtaank2003@gmail.com>
2024-10-10 17:09:45 +0530

    initial

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Usecase11 from "./components/Usecase11"
+function App() {
+
+  return (
+    <div className="container">
+      <h3 >Hello World</h3>
+      <Usecase11/>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Usecase11.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+function Usecase11() {
+  const [fullName,setfullName]=useState('Ayush Kumar')
+  const changeName=()=>{
+    setfullName('Ayush Kumar Tank')
+    
+  }
+  return (
+    <div>
+      <h3>Usecase1 : {fullName} </h3>
+      <button onClick={changeName}>click</button>
+    </div>
+    
+  )
+}
+
+export default Usecase11
